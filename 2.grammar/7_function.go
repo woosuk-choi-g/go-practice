b355c489f09cff4a820e597159214e8dd8e9bb84
woosuk.choi <woosuk.choi@influential.co.kr>
2023-08-20 02:41:20 +0000

    7_function

---
+++ 2.grammar/7_function.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func changeValue(message *string) {
+	*message = fmt.Sprintf("your name is %s", *message)
+}
+
+func addTen(value int) int {
+	return value + 10
+}
+
+// ... 은 slice 로 값을 받음
+func sum(values ...int) int {
+	result := 0
+	for _, value := range values {
+		result = result + value
+	}
+
+	return result
+}
+
+func divideStrings(value string) (string, string) {
+	values := strings.Split(value, "_")
+	return values[0], values[1]
+}
+
+func calc(f func(int, int) int, a int, b int) int {
+	result := f(a, b)
+	return result
+}
+
+func main() {
+	// 함수 호출
+	fmt.Println(addTen(100))
+
+	// slice 변수 호출
+	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	// 다중 변수 반환
+	prefix, suffix := divideStrings("Your_Name")
+	fmt.Println(prefix, suffix)
+
+	// 포인터 전달
+	message := "Ronaldo"
+	changeValue(&message)
+	fmt.Println(message)
+
+	// 익명 함수
+	sum := func(x int, y int) int {
+		return x + y
+	}
+	fmt.Println(sum(5, 10))
+
+	// 일급 함수
+	add := func(i int, j int) int {
+		return i + j
+	}
+
+	minus := func(i int, j int) int {
+		return i - j
+	}
+
+	fmt.Println(calc(add, 10, 5))
+	fmt.Println(calc(minus, 10, 5))
+}
